b712a1111c11f1d81c36cae510cee56bf0108f36
Wei-Chen Li <williamrob104@gmail.com>
2025-06-03 17:35:36 +0800

    Add test case for filament-rigid contact

--- geometry/proximity/test/filament_contact_internal_test.cc
+++ geometry/proximity/test/filament_contact_internal_test.cc
@@ -43,8 +43,7 @@ GTEST_TEST(FilamentContactInternalTest, FilamentSelfContact) {
   filament = Filament(closed, node_positions, cross_section);
   VectorXd q_WG(filament.node_pos().size() + filament.edge_m1().size());
   q_WG << filament.node_pos().reshaped(), filament.edge_m1().reshaped();
-  std::unordered_map<GeometryId, VectorXd> q_WGs = {{id, q_WG}};
-  engine.UpdateFilamentConfigurationVector(q_WGs);
+  engine.UpdateFilamentConfigurationVector({{id, q_WG}});
 
   /* Edge 0 and edge 2 now have a centerline distance of 0.8e-3. With a circular
    cross-section diameter of 1e-3, there should be a contact with 0.2e-3
@@ -106,8 +105,7 @@ GTEST_TEST(FilamentContactInternalTest, FilamentFilamentContact) {
   VectorXd q_WB(filament_B.node_pos().size() + filament_B.edge_m1().size());
   q_WB << filament_B.node_pos().reshaped(), filament_B.edge_m1().reshaped();
 
-  std::unordered_map<GeometryId, VectorXd> q_WGs = {{id_A, q_WA}, {id_B, q_WB}};
-  engine.UpdateFilamentConfigurationVector(q_WGs);
+  engine.UpdateFilamentConfigurationVector({{id_A, q_WA}, {id_B, q_WB}});
 
   /* The two filaments now have a centerline distance of 0.8e-3. With a circular
    cross-section diameter of 1e-3, there should be a contact with 0.2e-3
@@ -126,6 +124,75 @@ GTEST_TEST(FilamentContactInternalTest, FilamentFilamentContact) {
   EXPECT_TRUE(CompareMatrices(pair.p_WCs()[0], Vector3d(0, 0, 0), kTol));
   EXPECT_TRUE(CompareMatrices(pair.nhats_BA_W()[0], Vector3d(0, 0, -1), kTol));
   EXPECT_NEAR(pair.signed_distances()[0], -0.2e-3, kTol);
+}
+
+GTEST_TEST(FilamentContactInternalTest, FilamentRigidContact) {
+  ProximityEngine<double> engine;
+
+  math::RigidTransformd X_WG1(math::RotationMatrixd(), Vector3d(-0.6, 0, 0.3));
+  GeometryId id_G1 = GeometryId::get_new_id();
+  engine.AddDynamicGeometry(Sphere(0.3), X_WG1, id_G1);
+
+  math::RigidTransformd X_WG2(math::RotationMatrixd(), Vector3d(0.6, 0, 0.3));
+  GeometryId id_G2 = GeometryId::get_new_id();
+  engine.AddAnchoredGeometry(Sphere(0.3), X_WG2, id_G2);
+
+  const bool closed = false;
+  const double d = 1e-3;
+  const Filament::CircularCrossSection cross_section{.diameter = d};
+  Eigen::Matrix3Xd node_positions(3, 3);
+  node_positions.col(0) = Vector3d(-0.2, 0, 0);
+  node_positions.col(1) = Vector3d(0.0, 0, 0);
+  node_positions.col(2) = Vector3d(0.2, 0, 0);
+  Filament filament(closed, node_positions, cross_section);
+  GeometryId id_A = GeometryId::get_new_id();
+  engine.AddFilamentGeometry(filament, id_A);
+
+  /* The two shperes have no contact with the filament. */
+  FilamentContact<double> filament_contact;
+  engine.ComputeFilamentContact(&filament_contact);
+  ASSERT_EQ(filament_contact.contact_geometry_pairs().size(), 0);
+
+  /* Extend the filament to the left to touch sphere 1. */
+  node_positions.col(0) = Vector3d(-0.61, 0, 0);
+  filament = Filament(closed, node_positions, cross_section);
+  VectorXd q_WA(filament.node_pos().size() + filament.edge_m1().size());
+  q_WA << filament.node_pos().reshaped(), filament.edge_m1().reshaped();
+  engine.UpdateFilamentConfigurationVector({{id_A, q_WA}});
+
+  engine.ComputeFilamentContact(&filament_contact);
+  ASSERT_EQ(filament_contact.contact_geometry_pairs().size(), 1);
+
+  const FilamentContactGeometryPair<double>* pair =
+      &filament_contact.contact_geometry_pairs()[0];
+  EXPECT_EQ(pair->id_A(), id_A);
+  EXPECT_EQ(pair->id_B(), id_G1);
+  ASSERT_EQ(pair->num_contacts(), 1);
+  EXPECT_EQ(pair->contact_edge_indexes_A()[0], 0);
+  constexpr double kTol = 1e-10;
+  EXPECT_TRUE(
+      CompareMatrices(pair->p_WCs()[0], Vector3d(-0.6, 0, d / 4), kTol));
+  EXPECT_TRUE(CompareMatrices(pair->nhats_BA_W()[0], Vector3d(0, 0, -1), kTol));
+  EXPECT_NEAR(pair->signed_distances()[0], -d / 2, kTol);
+
+  /* Extend the filament to the right to touch sphere 2. */
+  node_positions.col(0) = Vector3d(-0.2, 0, 0);
+  node_positions.col(2) = Vector3d(0.61, 0, 0);
+  filament = Filament(closed, node_positions, cross_section);
+  q_WA << filament.node_pos().reshaped(), filament.edge_m1().reshaped();
+  engine.UpdateFilamentConfigurationVector({{id_A, q_WA}});
+
+  engine.ComputeFilamentContact(&filament_contact);
+  ASSERT_EQ(filament_contact.contact_geometry_pairs().size(), 1);
+
+  pair = &filament_contact.contact_geometry_pairs()[0];
+  EXPECT_EQ(pair->id_A(), id_A);
+  EXPECT_EQ(pair->id_B(), id_G2);
+  ASSERT_EQ(pair->num_contacts(), 1);
+  EXPECT_EQ(pair->contact_edge_indexes_A()[0], 1);
+  EXPECT_TRUE(CompareMatrices(pair->p_WCs()[0], Vector3d(0.6, 0, d / 4), kTol));
+  EXPECT_TRUE(CompareMatrices(pair->nhats_BA_W()[0], Vector3d(0, 0, -1), kTol));
+  EXPECT_NEAR(pair->signed_distances()[0], -d / 2, kTol);
 }
 
 }  // namespace
