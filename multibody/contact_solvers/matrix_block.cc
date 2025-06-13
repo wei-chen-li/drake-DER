81622c63985461f020d2d2299dfd598dd2948b78
Wei-Chen Li <williamrob104@gmail.com>
2025-06-13 17:25:49 +0800

    Refactor MatrixBlock<T>::LeftMultiplyByBlockDiagonal to use std::visit for handling dense and sparse matrix cases

--- multibody/contact_solvers/matrix_block.cc
+++ multibody/contact_solvers/matrix_block.cc
@@ -117,90 +117,28 @@ MatrixBlock<T> MatrixBlock<T>::LeftMultiplyByBlockDiagonal(
     DRAKE_DEMAND(Gs[i].rows() == Gs[i].cols());
     row_starts.emplace_back(row);
     row += Gs[i].rows();
-    if (!is_dense_) {
+    if (!is_dense()) {
       DRAKE_DEMAND(Gs[i].rows() % 3 == 0);
     }
   }
   DRAKE_DEMAND(row == rows());
 
-  if (is_dense_) {
-    const MatrixX<T>& M_dense = std::get<MatrixX<T>>(data_);
-    MatrixX<T> GM(rows(), cols());
-    for (int i = start; i <= end; ++i) {
-      const int rows = Gs[i].rows();
-      const int row_start = row_starts[i - start];
-      GM.middleRows(row_start, rows).noalias() =
-          Gs[i] * M_dense.middleRows(row_start, rows);
-    }
-    return MatrixBlock<T>(std::move(GM));
-  }
-
-  const Block3x3SparseMatrix<T>& M_sparse =
-      std::get<Block3x3SparseMatrix<T>>(data_);
-  /* At this point, we know that M is block 3x3 sparse and G is block diagonal,
-   G is block diagonal, with block h being 3nₕ x 3nₕ for positive integers nₕ.
-   Therefore, all quantities in the computation of A = G * M can be represented
-   as 3x3 blocks. We loop over all 3x3 non-zero blocks of M, and for each Mₖⱼ
-   (the k,j-th 3x3 block of M), we find all non-zero 3x3 blocks Gᵢₖ that
-   multiplies with Mₖⱼ, and add their product to the resulting 3x3 block, Aᵢⱼ.
-
-   Because G is block diagonal, to achieve the above, we only need:
-   (1) The index h into `Gs` to obtain the correct diagonal block of G that
-       multiplies with Mₖⱼ.
-   (2) The block row indices (i.e. the i's) of these non-zero 3x3 G subblocks.
-
-   Note that, for (2), the block row indices are contiguous, so we only need the
-   starting block row index and the size of that G block to determine all the
-   block row indices. */
-  std::vector<int> G_indices(
-      M_sparse.block_rows());  // `G_indices[k]` gives the relevant index h into
-                               // `Gs` for block column k.
-  std::vector<int> i_start(
-      M_sparse.block_rows());  // `i_start[k]` gives the starting block row
-                               // index from (2) above.
-  {
-    int i = 0;
-    int h = start;
-    for (int k = 0; k < M_sparse.block_rows(); ++k) {
-      if (3 * k >= row_starts[h - start] + Gs[h].rows()) {
-        i += Gs[h].cols() / 3;
-        ++h;
-      }
-      i_start[k] = i;
-      G_indices[k] = h;
-    }
-  }
-  /* We also record, for each k, the local block column index, l, such that the
-   `Gs[G_indices[k]].col(l)` gives the (non-zero entries of) k-th column of G.
-   */
-  std::vector<int> local_block_cols(M_sparse.block_rows());
-  for (int k = 0; k < M_sparse.block_rows(); ++k) {
-    local_block_cols[k] = k - i_start[k];
-  }
-
-  Block3x3SparseMatrix<T> A(M_sparse.block_rows(), M_sparse.block_cols());
-  using Triplet = typename Block3x3SparseMatrix<T>::Triplet;
-  const std::vector<std::vector<Triplet>>& M_triplets = M_sparse.get_triplets();
-  std::vector<Triplet> A_triplets;
-  A_triplets.reserve(M_sparse.num_blocks());
-  for (const auto& row_data : M_triplets) {
-    for (const Triplet& t : row_data) {
-      const int k = std::get<0>(t);
-      const int j = std::get<1>(t);
-      const Matrix3<T>& M_block = std::get<2>(t);
-      const int h = G_indices[k];
-      /* Given k and j, perform Aᵢⱼ+= Gᵢₖ * Mₖⱼ for all relevant i's. */
-      for (int l = 0; 3 * l < Gs[h].cols(); ++l) {
-        const int i = i_start[k] + l;
-        A_triplets.emplace_back(
-            i, j,
-            Gs[h].template block<3, 3>(3 * l, 3 * local_block_cols[k]) *
-                M_block);
-      }
-    }
-  }
-  A.SetFromTriplets(A_triplets);
-  return MatrixBlock<T>(std::move(A));
+  return std::visit(
+      overloaded{[&](const MatrixX<T>& M) {
+                   MatrixX<T> GM(rows(), cols());
+                   for (int i = start; i <= end; ++i) {
+                     const int rows = Gs[i].rows();
+                     const int row_start = row_starts[i - start];
+                     GM.middleRows(row_start, rows).noalias() =
+                         Gs[i] * M.middleRows(row_start, rows);
+                   }
+                   return MatrixBlock<T>(std::move(GM));
+                 },
+                 [&](const auto& M) {
+                   return MatrixBlock<T>(
+                       M.LeftMultiplyByBlockDiagonal(Gs, start, end));
+                 }},
+      data_);
 }
 
 template <class T>
